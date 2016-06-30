e907ffb870528135cadc1adb6fea40a996643301
William Schmitt <willj@schmittnet.com>
2016-06-29 20:03:47 -0400

    Added last_brewed and number_of_batches serializer values.

--- brewery/serializers.py
+++ brewery/serializers.py
@@ -20,9 +20,21 @@ class BrewerySerializer(serializers.ModelSerializer):
 
 class RecipeSerializer(serializers.ModelSerializer):
     style = serializers.SlugRelatedField(slug_field="name",queryset=BeerStyle.objects.all())
+    last_brewed = serializers.SerializerMethodField()
+    number_of_batches = serializers.SerializerMethodField()
     class Meta:
         model = Recipe
-        fields = ('name', 'style',)
+        fields = ('name', 'style','last_brewed','number_of_batches',)
+        
+    def get_last_brewed(self, obj):
+        recipe_instances = obj.recipeinstance_set
+        if recipe_instances.count()!=0:
+            return recipe_instances.latest('date').date
+        else:
+            return None
+        
+    def get_number_of_batches(self,obj):
+        return obj.recipeinstance_set.count()
 
 class TimeSeriesDataPointSerializer(serializers.ModelSerializer):
     class Meta:
