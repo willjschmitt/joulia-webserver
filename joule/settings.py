74face43ef2d7432d2e597f4d61ed571c4998a47
William Schmitt <willj@schmittnet.com>
2016-07-16 14:10:55 -0400

    Updated dockerrun files to use RDS instead of mysql instance.

--- joule/settings.py
+++ joule/settings.py
@@ -77,16 +77,28 @@ LOGIN_URL = '/login/'
 # Database
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'joule',
-        'USER': 'root',
-        'PASSWORD': 'joule',
-        'HOST': 'localhost',
-        'PORT': '',
+if 'RDS_HOSTNAME' in os.environ:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.mysql',
+            'NAME': os.environ['RDS_DB_NAME'],
+            'USER': os.environ['RDS_USERNAME'],
+            'PASSWORD': os.environ['RDS_PASSWORD'],
+            'HOST': os.environ['RDS_HOSTNAME'],
+            'PORT': os.environ['RDS_PORT'],
+        }
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.mysql',
+            'NAME': 'joule',
+            'USER': 'root',
+            'PASSWORD': 'joule',
+            'HOST': 'localhost',
+            'PORT': '',
+        }
+    }
 
 
 # Internationalization
