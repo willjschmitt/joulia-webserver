faf65a09a069dadfe83214b04caddadacf3bea84
William Schmitt <willj@schmittnet.com>
2016-07-17 18:32:29 -0400

    Adding admin views for relevant models.

--- joule/urls.py
+++ joule/urls.py
@@ -23,9 +23,12 @@ from .views import IndexView
 from django.conf import settings
 from django.conf.urls.static import static
 
+from django.contrib import admin
+
 urlpatterns = [
     url(r'^$', login_required(IndexView.as_view())),
     url(r'^', include('django.contrib.auth.urls')),
+    url(r'^admin/', admin.site.urls),
 #     url('^change-password/',auth_views.password_change,{'template_name': 'change-password.html'}),
     url('^', include('brewery.urls'))
 ]  + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
