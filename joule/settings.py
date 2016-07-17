cfcb9fe43238aa11ab530def126c8e41474f508e
William Schmitt <willj@schmittnet.com>
2016-07-17 19:36:01 -0400

    Making site info context processor more generic for site.

--- joule/settings.py
+++ joule/settings.py
@@ -68,7 +68,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
-                'joule.context_processors.site_name',
+                'joule.context_processors.site_info',
             ],
         },
     },
