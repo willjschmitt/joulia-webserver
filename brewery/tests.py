bd9b5559d7959ee6b3fe3718add8b1214a13ef5e
William Schmitt <willj@schmittnet.com>
2016-07-26 22:49:54 -0400

    Added some permissions checking tests.

--- brewery/tests.py
+++ brewery/tests.py
@@ -13,12 +13,13 @@ import json
 class BreweryTestCase(TestCase):
     @classmethod
     def setUpTestData(cls):
+        cls.c = Client()
+        
         user = User.objects.create_user("john","john@example.com","smith")
         group = Group.objects.create(name="Joulia Brewing Company")
         group.user_set.add(user)
         
-        cls.c = Client()
-        cls.c.post('/login/', {'username': 'john', 'password': 'smith'})
+        User.objects.create_user("alex","alex@example.com","notamember")
         
         cls.brewing_company = BrewingCompany.objects.create(group=group)
         cls.brewery = Brewery.objects.create(name="Main Facility",company=cls.brewing_company)
@@ -26,9 +27,16 @@ class BreweryTestCase(TestCase):
         
         style = BeerStyle.objects.create(name="American IPA")
         cls.recipe = Recipe.objects.create(name="Schmittfaced",style=style)
+        
+    def login_as_normal_user(self):
+        self.c.post('/login/', {'username': 'john', 'password': 'smith'})
+        
+    def login_as_alternate_user(self):
+        self.c.post('/login/', {'username': 'alex', 'password': 'notamember'})
 
 class RecipeInstanceLaunchTestCase(BreweryTestCase):
     def test_launch_instance_success(self):
+        self.login_as_normal_user()
         #start an instance
         launch_data = {'recipe':self.recipe.pk,
                        'brewhouse':self.brewhouse.pk}
@@ -38,6 +46,7 @@ class RecipeInstanceLaunchTestCase(BreweryTestCase):
         self.assertEqual(response.status_code, 200)
         
     def test_launch_instance_already_active(self):
+        self.login_as_normal_user()
         RecipeInstance.objects.create(recipe=self.recipe,
                                       brewhouse=self.brewhouse,
                                       active=True)
@@ -50,8 +59,22 @@ class RecipeInstanceLaunchTestCase(BreweryTestCase):
                                data=json.dumps(launch_data))
         self.assertEqual(response.status_code, 400)
         
+    def test_launch_instance_no_permission(self):
+        self.login_as_alternate_user()
+        RecipeInstance.objects.create(recipe=self.recipe,
+                                      brewhouse=self.brewhouse)
+        
+        #start an instance
+        launch_data = {'recipe':self.recipe.pk,
+                       'brewhouse':self.brewhouse.pk}
+        response = self.c.post('/brewery/brewhouse/launch',
+                               content_type="application/json",
+                               data=json.dumps(launch_data))
+        self.assertEqual(response.status_code, 403)
+        
 class RecipeInstanceEndTestCase(BreweryTestCase):
     def test_end_instance_success(self):
+        self.login_as_normal_user()
         recipe_instance = RecipeInstance.objects.create(recipe=self.recipe,
                                                         brewhouse=self.brewhouse,
                                                         active=True)
@@ -64,6 +87,7 @@ class RecipeInstanceEndTestCase(BreweryTestCase):
         self.assertEqual(response.status_code, 200)
         
     def test_end_instance_already_active(self):
+        self.login_as_normal_user()
         recipe_instance = RecipeInstance.objects.create(recipe=self.recipe,
                                                         brewhouse=self.brewhouse,
                                                         active=False)
@@ -74,3 +98,15 @@ class RecipeInstanceEndTestCase(BreweryTestCase):
                                content_type="application/json",
                                data=json.dumps(end_data))
         self.assertEqual(response.status_code, 400)
+        
+    def test_end_instance_no_permission(self):
+        self.login_as_alternate_user()
+        recipe_instance = RecipeInstance.objects.create(recipe=self.recipe,
+                                                        brewhouse=self.brewhouse)
+        
+        #end the instance
+        end_data = {'recipe_instance':recipe_instance.pk}
+        response = self.c.post('/brewery/brewhouse/end',
+                               content_type="application/json",
+                               data=json.dumps(end_data))
+        self.assertEqual(response.status_code, 403)
