cb77f9d47adbb8212e099f9e72bf1b08ef58c5f7
William Schmitt <willj@schmittnet.com>
2017-04-06 20:27:25 -0700

    Updated urls.

--- joule/urls.py
+++ joule/urls.py
@@ -1,34 +1,13 @@
-"""joule URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/1.8/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
-Including another URLconf
-    1. Add an import:  from blog import urls as blog_urls
-    2. Add a URL to urlpatterns:  url(r'^blog/', include(blog_urls))
-"""
-
-from django.conf.urls import url,include
-
-from django.contrib.auth.decorators import login_required
-
-from .views import IndexView
-
+from django.conf.urls import url, include
 from django.conf import settings
 from django.conf.urls.static import static
-
 from django.contrib import admin
+from django.views.generic import TemplateView
 
 import social_django.views as social_views
 
 urlpatterns = [
-    url(r'^$', login_required(IndexView.as_view())),
+    url(r'^$', TemplateView.as_view(template_name="index.html")),
     url(r'^', include('django.contrib.auth.urls')),
     url(r'^admin/', admin.site.urls),
     url('^', include('brewery.urls')),
@@ -36,4 +15,4 @@ urlpatterns = [
     url('', include('social_django.urls', namespace='social')),
     url(r'^login/google-oauth2/$', social_views.auth,
         name='login-google-oauth2')
-]  + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
