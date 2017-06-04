2acd5c08d2c5a0c2fd23818a3dfa136dff2c1605
William Schmitt <willj@schmittnet.com>
2017-06-04 14:58:07 -0700

    Logging config for django streams everything.

--- joulia/settings.py
+++ joulia/settings.py
@@ -173,3 +173,20 @@ REST_FRAMEWORK = {
     ),
     'COERCE_DECIMAL_TO_STRING': False,
 }
+
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'root': {
+            'handlers': ['console'],
+            'level': os.getenv('DJANGO_LOG_LEVEL', 'INFO'),
+        },
+    },
+}
