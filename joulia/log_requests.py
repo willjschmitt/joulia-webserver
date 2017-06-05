4f1f0594c9decc7ab884bc5acad7f1284925ca0e
William Schmitt <willj@schmittnet.com>
2017-06-04 22:32:40 -0700

    Better headers logging.

--- joulia/log_requests.py
+++ joulia/log_requests.py
@@ -2,35 +2,36 @@
 headers of a request for debugging things like authentication behind proxies.
 """
 
-import re
 from http.client import responses
 import logging
 
-req_log = logging.getLogger(__name__)
+LOGGER = logging.getLogger(__name__)
 
 
 class HeadersLoggingMiddleware(object):
-    def process_response(self, request, response):
-        keys = sorted(
-            filter(
-                lambda k: re.match(r'(HTTP_|CONTENT_)', k), request.META))
-        keys = ['REMOTE_ADDR'] + keys
-        meta = ''.join("%s=%s\n" % (k, request.META[k]) for k in keys)
-
+    @staticmethod
+    def process_response(request, response):
         try:
             status_text = responses[response.status_code]
         except KeyError:
             status_text = 'UNKNOWN STATUS CODE'
-        status = '%s %s' % (response.status_code, status_text)
-        response_headers = [(str(k), str(v)) for k, v in response.items()]
-        for c in response.cookies.values():
-            response_headers.append(
-                ('Set-Cookie', str(c.output(header=''))))
-        headers = ''.join("%s: %s\n" % c for c in response_headers)
-        log_entry = '"%s %s\n%s\n%s\n%s' % (request.method,
-                                            request.build_absolute_uri(),
-                                            meta, status, headers)
-        req_log.info('"%s %s\n%s\n%s\n%s' % (request.method,
-                                             request.build_absolute_uri(),
-                                             meta, status, headers))
+        status = '{} {}'.format(response.status_code, status_text)
+        request_string = "{} {} {}".format(request.method, status,
+                                           request.build_absolute_uri())
+
+        request_headers = []
+        for k, v in sorted(request.META.items()):
+            request_headers.append('{}={}'.format(k, v))
+        request_headers = '\n'.join(request_headers)
+
+        response_headers = []
+        for k, v in sorted(response.items()):
+            response_headers.append("{}:{}".format(k, v))
+        response_headers = '\n'.join(response_headers)
+
+        cookies = '\n'.join(c.output() for c in response.cookies.values())
+
+        LOGGER.info('"%s\nRequest:\n%s\nResponse:\n%s\nCookies:\n%s',
+                    request_string, request_headers, response_headers, cookies)
+
         return response
