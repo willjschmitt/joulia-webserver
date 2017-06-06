3cb7caf87d06eb36f68fac1e2b950f155b7fd426
William Schmitt <willj@schmittnet.com>
2017-06-05 18:07:35 -0700

    Updating to new middleware style.

--- joulia/settings.py
+++ joulia/settings.py
@@ -49,7 +49,7 @@ INSTALLED_APPS = (
     'brewery',
 )
 
-MIDDLEWARE_CLASSES = (
+MIDDLEWARE = (
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
