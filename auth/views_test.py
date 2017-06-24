ce1ea58822b0189c08f97173b972a72c541e0070
William Schmitt <willj@schmittnet.com>
2017-06-24 14:56:56 -0700

    Fixing test for user views.

--- auth/views_test.py
+++ auth/views_test.py
@@ -14,14 +14,15 @@ class TestUserView(TestCase):
         self.c = Client()
 
     def test_get_user_logged_in(self):
-        User.objects.create_user("john", "john@example.com", "smith",
-                                 first_name="john", last_name="smith")
+        user = User.objects.create_user("john", "john@example.com", "smith",
+                                        first_name="john", last_name="smith")
         self.c.post('/login/', {'username': 'john', 'password': 'smith'})
         response = self.c.get('/auth/api/user/',
                               content_type="application/json")
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         response_data = response.json()
         want = {
+            "id": user.pk,
             "first_name": "john",
             "last_name": "smith",
             "email": "john@example.com",
