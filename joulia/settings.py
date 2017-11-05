bfc0f8a5709be1086fa0dd5b951e2528656f539b
William Schmitt <willj@schmittnet.com>
2017-11-04 17:05:16 -0700

    Update logging settings to log better.

--- joulia/settings.py
+++ joulia/settings.py
@@ -188,24 +188,48 @@ REST_FRAMEWORK = {
     'COERCE_DECIMAL_TO_STRING': False,
 }
 
-JOULIA_LOG_FILE = os.path.join(BASE_DIR, 'joulia.log')
 if not TRAVIS and PRODUCTION_HOST:
-    JOULIA_LOG_FILE = '/var/log/joulia.log'
+    LOGGING_DIR = "/var/log/joulia"
+else:
+    LOGGING_DIR = os.path.join(os.getcwd(), "log")
 
-LOGGING = {
+# Make sure the logging directory exists.
+if not os.path.exists(LOGGING_DIR):
+    os.makedirs(LOGGING_DIR)
+
+LOGGING_CONFIG = {
     'version': 1,
     'disable_existing_loggers': False,
-    'handlers': {
-        'file': {
-            'level': 'DEBUG',
-            'class': 'logging.FileHandler',
-            'filename': JOULIA_LOG_FILE,
+    'formatters': {
+        'standard': {
+            'format': '%(levelname)-8s %(asctime)s %(name)-12s %(message)s'
         },
     },
-    'loggers': {},
-    'root': {
-        'handlers': ['file'],
-        'level': 'DEBUG',
-        'propagate': True,
+    'handlers': {
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+            'formatter': 'standard',
+        },
+        'file': {
+            'level': 'DEBUG',
+            'class': 'logging.handlers.RotatingFileHandler',
+            'formatter': 'standard',
+            'filename': os.path.join(LOGGING_DIR, 'joulia.log'),
+            'maxBytes': 10 * 1024 * 1024,  # 10MB logfiles.
+            'backupCount': 10,
+        },
+    },
+    'loggers': {
+        '': {
+            'level': 'DEBUG',
+            'handlers': ['console', 'file'],
+            'propagate': True
+        },
+        'requests': {
+            'level': 'ERROR',
+            'handlers': ['console', 'file'],
+            'propagate': False
+        },
     },
 }
