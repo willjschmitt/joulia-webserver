f9907dd7d188dd1f525488d8e42945a2194c4493
William Schmitt <willj@schmittnet.com>
2017-12-09 21:06:00 -0500

    Adding more in after success

--- deploy_docker.sh
+++ deploy_docker.sh
@@ -1,2 +1,14 @@
 #!/bin/bash
-echo "test"
+# Deploys the application to Docker by building the docker image, and pushing it
+# to Docker. Tags the image with the first 6 characters of the commit, "latest",
+# and the Travis build number.
+# Expects the following environment variables to be provided by travis:
+#  * TRAVIS_COMMIT
+#  * DOCKER_USERNAME
+#  * DOCKER_PASSWORD
+#  * TRAVIS_BUILD_NUMBER
+echo "Deploying Docker image."
+
+DOCKER_REPO="willjschmitt/joulia-webserver"
+COMMIT="${TRAVIS_COMMIT}::6"
+
