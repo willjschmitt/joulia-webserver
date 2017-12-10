1b5265ada45cc812aec79e01268b0215ef114524
William Schmitt <willj@schmittnet.com>
2017-12-09 19:32:38 -0500

    Adding more graceful error handling to deploy script.

--- deploy_docker.sh
+++ deploy_docker.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash -e
+#!/usr/bin/env bash
 # Deploys the application to Docker by building the docker image, and pushing it
 # to Docker. Tags the image with the first 6 characters of the commit, "latest",
 # and the Travis build number.
@@ -10,8 +10,30 @@
 DOCKER_REPO="willjschmitt/joulia-webserver"
 COMMIT="${TRAVIS_COMMIT}::6"
 
+echo "Logging into Docker"
 docker login -u "${DOCKER_USERNAME}" -p "${DOCKER_PASSWORD}"
+if [ $? -ne 0 ]; then
+  echo "Failed to log into Docker."
+fi
+
+echo "Building Docker image."
 docker build -f Dockerfile -t "${DOCKER_REPO}:${COMMIT}" .
+if [ $? -ne 0 ]; then
+  echo "Failed to build Docker image."
+fi
+
+echo "Tagging Docker image."
 docker tag "${DOCKER_REPO}:${COMMIT}" "${DOCKER_REPO}:latest"
+if [ #? -ne 0 ]; then
+  echo "Failed to tag Docker image with latest tag."
+fi
 docker tag "${DOCKER_REPO}:${COMMIT}" "${DOCKER_REPO}:travis-${TRAVIS_BUILD_NUMBER}"
+if [ #? -ne 0 ]; then
+  echo "Failed to tag Docker image with travis build number."
+fi
+
+echo "Pushing Docker image."
 docker push "${DOCKER_REPO}"
+if [ #? -ne 0 ]; then
+  echo "Failed to push Docker image to Docker."
+fi
