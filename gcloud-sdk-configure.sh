2a4d524dc0f8c4fb1f177ff9b356f4d764d60918
William Schmitt <willj@schmittnet.com>
2017-12-11 20:18:02 -0500

    Install instead of update kubectl

--- gcloud-sdk-configure.sh
+++ gcloud-sdk-configure.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -e
 # Configures and connects to Google Cloud SDK using encrypted client secret.
 
-gcloud components update kubectl
+gcloud components install kubectl
 gcloud auth activate-service-account --key-file "${GCLOUD_APPLICATION_CREDENTIALS}"
 gcloud config set project "${GCLOUD_PROJECT_ID}"
 gcloud config set compute/zone "${GCLOUD_ZONE}"
