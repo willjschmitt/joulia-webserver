8fc57216e3dabd9fab236c1fd057b20141f4732b
William Schmitt <willj@schmittnet.com>
2017-12-11 20:23:03 -0500

    Remove kubectl from gcloud configuration.

--- gcloud-sdk-configure.sh
+++ gcloud-sdk-configure.sh
@@ -1,7 +1,6 @@
 #!/bin/bash -e
 # Configures and connects to Google Cloud SDK using encrypted client secret.
 
-gcloud components install kubectl
 gcloud auth activate-service-account --key-file "${GCLOUD_APPLICATION_CREDENTIALS}"
 gcloud config set project "${GCLOUD_PROJECT_ID}"
 gcloud config set compute/zone "${GCLOUD_ZONE}"
